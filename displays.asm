df8328d0cccca75fc2bfc549ad81ad65972709d5
fabianblatz <fabian.blatz@hansgrohe.de>
2017-06-08 16:01:10 +0200

    lel

--- displays.asm
+++ displays.asm
@@ -1,32 +1,36 @@
-; Add 'ret' to each display to stop looping
-display_0_0:
-mov p0,#10000001b ; Die Null
-acall delay
-display_0_1:
-mov p0,#11001111b ; Die Oins
-acall delay
-display_0_2:
-mov p0,#10010010b ; Die Zwo
-acall delay
-display_0_3:
-mov p0,#10000110b ; Die Drey
-acall delay
-display_0_4:
-mov p0,#11001100b ; Die Vier
-acall delay
-display_0_5:
-mov p0,#10100100b ; Die Fümf
-acall delay
-display_0_6:
-mov p0,#10100000b ; Die Serks
-acall delay
-display_0_7:
-mov p0,#10001111b ; Die Sieben
-acall delay
-display_0_8:
-mov p0,#10000000b ; Die Agt
-acall delay
-display_0_9:
-mov p0,#10000100b ; Die Noin
-acall delay
-sjmp repeat ; remove to stop looping
+display15right:
+SETB P1.1
+clr P0.0
+mov R6, #0fh
+acall meme
+
+display30right:
+SETB P1.2
+clr P0.0
+mov R6, #1eh
+acall meme
+
+display40right:
+SETB P1.3
+clr P0.0
+mov R6, #28h
+acall meme
+
+
+display15left:
+SETB P1.5
+clr P0.1
+mov R7, #0fh
+acall meme
+
+display30left:
+SETB P1.6
+clr P0.1
+mov R7, #1eh
+acall meme
+
+display40left:
+SETB P1.7
+clr P0.1
+mov R7, #28h
+acall meme
