9be6e4651660e726c5c10c89e131878221ef0b58
fabianblatz <fabian.blatz@hansgrohe.de>
2017-06-08 12:28:35 +0200

    implemented multiplex displaying the values in R7 and R6

--- main_file.asm
+++ main_file.asm
@@ -1,25 +1,79 @@
-include displays.asm
-org 0000h
+;Countdown / Zaehler auf Null
+;in Minuten und Sekunden
+;
+; mit 4x7-segment anzeige
+;
+; initialisierung
+cseg at 0h
+ajmp init
+cseg at 100h
+
+
+ORG 0bh
 
 
 
-repeat:
-acall display_0_0
-acall display_0_1
-acall display_0_2
-acall display_0_3
-acall display_0_4
-acall display_0_5
-acall display_0_6
-acall display_0_7
-acall display_0_8
-acall display_0_9
-delay:
-;mov r3,#010h
-;l3:mov r2,#0ffh
-;l2:mov r1,#0ffh
-mov r1,#010h
-l1:djnz r1,l1
-;djnz r2,l2
-;djnz r3,l3
-ret 
+ORG 20h
+init:
+mov IE, #10010010b
+mov tmod, #00000010b
+
+mov r6, #05h
+acall zeigen
+sjmp init
+
+
+
+zeigen:
+mov DPTR, #table
+mov a, R6
+mov b, #0ah
+div ab
+mov R0, a
+movc a,@a+dptr
+mov r3, a
+mov a, r0
+xch a,b
+movc a, @a+dptr
+mov r2, a
+;----------------
+mov a, R7
+mov b, #0ah
+div ab
+mov R0, a
+movc a,@a+dptr
+mov r5, a
+mov a, r0
+xch a,b
+movc a, @a+dptr
+mov r4, a
+call display
+ret
+
+display:
+mov P3, R2
+clr P2.0
+setb P2.0
+
+mov P3, R3
+clr P2.1
+setb P2.1
+
+mov P3, R4
+clr P2.2
+setb P2.2
+
+mov P3, R5
+clr P2.3
+setb P2.3
+
+ret
+
+org 300h
+table:
+db 11000000b
+db 11111001b, 10100100b, 10110000b
+db 10011001b, 10010010b, 10000010b
+db 11111000b, 10000000b, 10010000b
+
+end
